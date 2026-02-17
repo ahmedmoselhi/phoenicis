753bbf5cb2f03908fc566086260fb10e052d5f16
ahmedmoselhi <ahmedmoselhi55@gmail.com>
2026-02-17 22:05:26 +0200

    Remove WineHQ-specific traces from installer sanitization

--- phoenicis-javafx/src/test/java/org/phoenicis/javafx/components/application/skin/ApplicationInformationPanelSkinSourceTest.java
+++ phoenicis-javafx/src/test/java/org/phoenicis/javafx/components/application/skin/ApplicationInformationPanelSkinSourceTest.java
@@ -7,16 +7,15 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ApplicationInformationPanelSkinSourceTest {
     @Test
-    public void testWineHqSourceUrlIsNotReferenced() throws IOException {
+    public void testUsesPlayOnLinuxWineBinariesUrl() throws IOException {
         final Path sourcePath = Path.of(
                 "src/main/java/org/phoenicis/javafx/components/application/skin/ApplicationInformationPanelSkin.java");
         final String content = Files.readString(sourcePath, StandardCharsets.UTF_8);
 
-        assertFalse(content.contains("https://dl.winehq.org/wine/source/"));
-        assertFalse(content.contains("Could not read any Wine branch"));
+        assertTrue(content.contains("http://www.playonlinux.com/wine/binaries/"));
     }
 }
